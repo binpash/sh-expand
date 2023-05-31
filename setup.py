375b09ad7b5ca34c59c4481068dff3a34e133c1e
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-05-31 11:08:01 -0400

    requirements and setup

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import setup
 from pathlib import Path
 long_description = (Path(__file__).parent / "README.md").read_text()
 
+def get_install_requires() -> "list[str]":
+    """Returns requirements.txt parsed to a list"""
+    fname = Path(__file__).parent / 'requirements.txt'
+    targets = []
+    if fname.exists():
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
+
 setup(name='sh-expand',
       version='0.1.0',
       packages=['sh_expand'],
@@ -11,4 +20,5 @@ setup(name='sh-expand',
       long_description_content_type="text/markdown",
       python_requires='>=3.8',
       include_package_data=True,
+      install_requires=get_install_requires(),
       )
