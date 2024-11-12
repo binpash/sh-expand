b51609a95d401f0451b18da1016b126bc7ef6aad
Bolun Thompson <abolunthompson@gmail.com>
2024-11-12 00:53:23 +0000

    Add: query bash version for read_vars_file
    Signed-off-by: Bolun Thompson <abolunthompson@gmail.com>

--- sh_expand/env_vars_util.py
+++ sh_expand/env_vars_util.py
@@ -1,11 +1,25 @@
 import shlex
+import subprocess
+
 from datetime import datetime
 
 from sh_expand.util import log, print_time_delta
 
+def get_bash_version():
+    try:
+        out = subprocess.run(["bash", "-c", "echo ${BASH_VERSINFO[@]}"])
+    # bash isn't on the system
+    except FileNotFoundError:
+        return None
+    return tuple(int(v) for v in out.stdout.split(b" ")[:4])
+
 def read_vars_file(var_file_path, bash_version_tuple = None):
     if var_file_path is not None:
         log(f'Reading variables from: {var_file_path}')
+
+        if bash_version_tuple is None:
+            bash_version_tuple = get_bash_version()
+
         if bash_version_tuple is not None and bash_version_tuple < (5, 2, 0):
             vars_dict = read_vars_file_old(var_file_path)
         else:
